729fc3219362403ebc7a56d8a17493a224e1ec7e
Iain C Docherty <github@iain-c-docherty.com>
2013-12-15 17:45:03 +0000

    Added script to install all Perl modules

---
+++ bin/setup/install_perl_mods.sh
@@ -0,0 +1,22 @@
+cpanm Plack
+cpanm Plack::App::WebSocket::Connection
+cpanm JSON
+cpanm AnyEvent::WebSocket::Server
+cpanm Server::Starter
+cpanm Moose
+cpanm namespace::autoclean
+cpanm Config::JSON
+cpanm Module::Find
+cpanm Redis
+cpanm UUID::Tiny
+cpanm DBIx::Class
+cpanm Crypt::SaltedHash
+cpanm DBIx::Class::TimeStamp
+cpanm DBIx::Class::InflateColumn::Serializer
+cpanm Data::Validate::Email
+cpanm Mojo::JSON
+cpanm File::Next
+cpanm Plack::Handler::Twiggy
+cpanm Try
+cpanm DBB::mysql
+
